9413d376497b1a059fa47b5f271fb7882c6de98e
Nicki-afk <casperplay45@gmail.com>
2023-07-28 22:33:42 +0300

    init commit

---
+++ src/main/java/gyber/websocket/client/ClientEndpointClass.java
@@ -0,0 +1,63 @@
+package gyber.websocket.client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Scanner;
+
+import javax.websocket.*;
+
+import gyber.websocket.codecs.MessageDecoder;
+import gyber.websocket.codecs.MessageEncoder;
+
+
+@ClientEndpoint(encoders = MessageEncoder.class , decoders = MessageDecoder.class)
+public class ClientEndpointClass {
+
+
+     private Session session;
+
+
+
+    @OnOpen
+    public void onOpen(Session session) {
+
+        this.session = session;
+        System.out.println("Connected! ");
+
+        //session.setMaxIdleTimeout(10000);
+        
+
+        try {
+
+            Thread.sleep(2000);
+           System.out.println("IS Open : " + session.isOpen());
+
+            System.out.print(" >> ");
+
+            String msg = new Scanner(System.in).nextLine();
+            System.out.println("MSG = " + msg);
+
+            Message message = new Message("gyber", "gyber", msg);
+
+
+
+            session.getBasicRemote().sendObject(message);
+            System.out.println("IS Open : " + session.isOpen());
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        try{
+        System.out.println("Received: " + new MessageDecoder().decode(message).getContent());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    
+}
---
+++ src/main/java/gyber/websocket/main/MainClass.java
@@ -0,0 +1,45 @@
+package gyber.websocket.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Scanner;
+
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+import gyber.websocket.client.ClientEndpointClass;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        
+        try {       
+            URI uri = URI.create("ws://localhost:8080/gyberwebsocket-0.0.2-inside-test/chat/gyber?gyber");
+            ClientEndpointClass endpoint = new ClientEndpointClass();
+
+            Session session = container.connectToServer(ClientEndpointClass.class, uri);
+
+            
+            Thread.sleep(300);
+
+            session.getBasicRemote().getSendStream().close();
+            session.close();
+        
+
+            // session.close();
+            
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+        }
+    }
+
+
+    
+
+    
+}
