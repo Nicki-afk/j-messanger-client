af25f451cf110d83cd7cd9ce3d7556e2b9de22d2
Nicki-afk <casperplay45@gmail.com>
2023-07-28 23:13:36 +0300

    Добавлен метод в логгер для клиентских логов

--- src/main/java/gyber/websocket/client/LogMessage.java
+++ src/main/java/gyber/websocket/client/LogMessage.java
@@ -37,5 +37,35 @@ public class LogMessage {
     
 
     }
+
+    public static void logClientMessage(String message){
+
+        try{
+
+            
+            char[]chars = message.toCharArray();
+
+            System.out.print("[  " + "CLIENT" + "  ]  : " );
+
+        
+            int x = 0;
+            while(x < chars.length){
+                
+                System.out.print(chars[x]);  // Печатает один символ в секунду
+
+                x++;
+                Thread.sleep(1000);
+            }
+
+            System.out.println();
+            
+       }catch(Exception e){
+            e.printStackTrace();
+
+       }
+
+
+
+    }
     
 }
