9c4e995e70c75b5dd35c705e69dbe6c59ee9582b
Nicki-afk <casperplay45@gmail.com>
2023-07-30 15:18:39 +0300

    Изменен процесс вывода сообщений

--- src/main/java/gyber/websocket/client/LogMessage.java
+++ src/main/java/gyber/websocket/client/LogMessage.java
@@ -1,5 +1,7 @@
 package gyber.websocket.client;
 
+import java.util.Arrays;
+
 import gyber.websocket.codecs.MessageDecoder;
 
 // TODO : Сделать перегрузку методов и убрать дублирующийся код
@@ -7,151 +9,343 @@ public class LogMessage {
 
 
 
+    public static boolean stylePrint = true;  // Переменная отвечает за стиль отрисовки текста если она true то текст будет выводится по буквенно если нет то обычным текстом
 
+
+
+
+    // Для отрисовки сообщений 
     public static void logMessage(String message){
-
-       
 
         try{
 
-            Message msg = new MessageDecoder().decode(message);
-            char[]chars = msg.getContent().toCharArray();
+            if(stylePrint){
 
-            System.out.print(msg.getContent().length() > 70 ? "\n[  " + msg.getFrom() + "  ]  : " : "[  " + msg.getFrom() + "  ]  : " );
-        
-        
+                    Message msg = new MessageDecoder().decode(message);
+                    char[]chars = msg.getContent().toCharArray();
 
-            int x = 0;
-            int charSpace = 0;
-            while(x < chars.length){
-
-               
+                    System.out.print(msg.getContent().length() > 70 ? "\n[  " + msg.getFrom() + "  ]  : " : "[  " + msg.getFrom() + "  ]  : " );
                 
-                if(charSpace >= 110){
-                                     
-                    System.out.println();
-                    System.out.print("                " + chars[x]);
-                    Thread.sleep(20);
-                    charSpace = 0;
-                    x++;
                 
 
-                }else{
-
-                    System.out.print(chars[x]);
+                    int x = 0;
+                    int charSpace = 0;
+                    while(x < chars.length){
 
                     
-                    x++;
-                    charSpace++;
-                    Thread.sleep(20);
-                }
+                        
+                        if(charSpace >= 110){
+                                            
+                            System.out.println();
+                            System.out.print("                " + chars[x]);
+                            Thread.sleep(20);
+                            charSpace = 0;
+                            x++;
+                        
+
+                        }else{
+
+                            System.out.print(chars[x]);
+
+                            
+                            x++;
+                            charSpace++;
+                            Thread.sleep(20);
+                        }
+
+                    }
+
+
+
+            }else{
+
+                Message msg = new MessageDecoder().decode(message);
+                char[]chars = msg.getContent().toCharArray();
+
+                 System.out.print(msg.getContent().length() > 70 ? "\n[  " + msg.getFrom() + "  ]  : " : "[  " + msg.getFrom() + "  ]  : " );
+                
+                
+
+                    int x = 0;
+                    int charSpace = 0;
+                    while(x < chars.length){
+
+                    
+                        
+                        if(charSpace >= 110){
+                                            
+                            System.out.println();
+                            System.out.print("                " + chars[x]);
+                            //Thread.sleep(20);
+                            charSpace = 0;
+                            x++;
+                        
+
+                        }else{
+
+                            System.out.print(chars[x]);
+
+                            
+                            x++;
+                            charSpace++;
+                            Thread.sleep(20);
+                        }
+
+                    }
+
+
+
+
 
             }
 
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    // Для отрисовки клиентских сообщений
+    public static void logMessage(String message , boolean haveInput){
+
+        try{
+
+            if(stylePrint){
+                    char[]chars = message.toCharArray();
+                   System.out.print("[  " + "CLIENT" + "  ]  : " );
+                
+                
+
+                    int x = 0;
+                    int charSpace = 0;
+                    while(x < chars.length){
+
+                    
+                        
+                        if(charSpace >= 110){
+                                            
+                            System.out.println();
+                            System.out.print("                " + chars[x]);
+                            Thread.sleep(20);
+                            charSpace = 0;
+                            x++;
+                        
+
+                        }else{
+
+                            System.out.print(chars[x]);
+
+                            
+                            x++;
+                            charSpace++;
+                            Thread.sleep(20);
+                        }
+
+                    }
+
+                    if(!haveInput){
+                        System.out.println();
+
+                    }
+
+
+
+            }else{
+
+                String words[] = message.split(" ");
+
+                // Расставляем переносы стороки 
+                int newLineIndex = 0;
+                for(int x = 0;  x < words.length; x++){
+                    if(newLineIndex == 10){
+                        words[x] = words[x].concat(" \n".concat("               "));
+                        newLineIndex = 0;
+
+                    }
+                    newLineIndex++;
+
+
+                }
+
+                System.out.print("[  " + "CLIENT" + "  ]  : " );
+
+                // Выводим 
+                for(String i : words){
+                    System.out.print(i + " ");
+                }
+
+
+                if(!haveInput){
+                    System.out.println();
+
+                }
+
+
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    // public static void logMessage(String message){
+
+       
+
+    //     try{
+
+    //         Message msg = new MessageDecoder().decode(message);
+    //         char[]chars = msg.getContent().toCharArray();
+
+    //         System.out.print(msg.getContent().length() > 70 ? "\n[  " + msg.getFrom() + "  ]  : " : "[  " + msg.getFrom() + "  ]  : " );
+        
+        
+
+    //         int x = 0;
+    //         int charSpace = 0;
+    //         while(x < chars.length){
+
+               
+                
+    //             if(charSpace >= 110){
+                                     
+    //                 System.out.println();
+    //                 System.out.print("                " + chars[x]);
+    //                 Thread.sleep(20);
+    //                 charSpace = 0;
+    //                 x++;
+                
+
+    //             }else{
+
+    //                 System.out.print(chars[x]);
+
+                    
+    //                 x++;
+    //                 charSpace++;
+    //                 Thread.sleep(20);
+    //             }
+
+    //         }
+
             
 
-            System.out.println();
+    //         System.out.println();
            
             
-       }catch(Exception e){
-            e.printStackTrace();
+    //    }catch(Exception e){
+    //         e.printStackTrace();
 
-       }
+    //    }
 
       
     
 
-    }
+    // }
 
-    public static void logClientMessage(String message){
+    // public static void logClientMessage(String message){
 
-        try{
+    //     try{
 
             
-            char[]chars = message.toCharArray();
+    //         char[]chars = message.toCharArray();
 
-            System.out.print("[  " + "CLIENT" + "  ]  : " );
+    //         System.out.print("[  " + "CLIENT" + "  ]  : " );
 
         
-            int x = 0;
-            int charSpace = 0;
-            while(x < chars.length){
+    //         int x = 0;
+    //         int charSpace = 0;
+    //         while(x < chars.length){
 
-                if(charSpace >= 110){
+    //             if(charSpace >= 110){
                                      
-                    System.out.println();
-                    System.out.print("                " + chars[x]);
-                    Thread.sleep(20);
-                    charSpace = 0;
-                    x++;
+    //                 System.out.println();
+    //                 System.out.print("                " + chars[x]);
+    //                 Thread.sleep(20);
+    //                 charSpace = 0;
+    //                 x++;
                 
 
-                }else{
+    //             }else{
 
-                    System.out.print(chars[x]);
+    //                 System.out.print(chars[x]);
 
                     
-                    x++;
-                    charSpace++;
-                    Thread.sleep(20);
-                }
+    //                 x++;
+    //                 charSpace++;
+    //                 Thread.sleep(20);
+    //             }
                 
-            }
+    //         }
 
-            System.out.println();
+    //         System.out.println();
             
-       }catch(Exception e){
-            e.printStackTrace();
+    //    }catch(Exception e){
+    //         e.printStackTrace();
 
-       }
-
-
-
-    }
-
-    public static void logPrintClientMessage(String mString){
+    //    }
 
 
-        try{
+
+    // }
+
+    // public static void logPrintClientMessage(String mString){
+
+
+    //     try{
 
             
-            char[]chars = mString.toCharArray();
+    //         char[]chars = mString.toCharArray();
 
-            System.out.print("[  " + "CLIENT" + "  ]  : " );
+    //         System.out.print("[  " + "CLIENT" + "  ]  : " );
 
         
-            int x = 0;
-            int charSpace = 0;
-            while(x < chars.length){
+    //         int x = 0;
+    //         int charSpace = 0;
+    //         while(x < chars.length){
 
-                if(charSpace >= 110){
+    //             if(charSpace >= 110){
                                      
-                    System.out.println();
-                    System.out.print("                " + chars[x]);
-                    Thread.sleep(20);
-                    charSpace = 0;
-                    x++;
+    //                 System.out.println();
+    //                 System.out.print("                " + chars[x]);
+    //                 Thread.sleep(20);
+    //                 charSpace = 0;
+    //                 x++;
                 
 
-                }else{
+    //             }else{
 
-                    System.out.print(chars[x]);
+    //                 System.out.print(chars[x]);
 
                     
-                    x++;
-                    charSpace++;
-                    Thread.sleep(20);
-                }
+    //                 x++;
+    //                 charSpace++;
+    //                 Thread.sleep(20);
+    //             }
                 
-            }
+    //         }
 
-            // System.out.println();
+    //         // System.out.println();
             
-       }catch(Exception e){
-            e.printStackTrace();
+    //    }catch(Exception e){
+    //         e.printStackTrace();
 
-       }
+    //    }
 
-    }
+    // }
     
 }
